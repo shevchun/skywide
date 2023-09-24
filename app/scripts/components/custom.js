ed37f0688cb357ac4251c07e85db82a069667fbf
Vladimir Shevchun <vovashevchun@gmail.com>
2023-09-24 15:23:36 +0200

    work

--- app/scripts/components/custom.js
+++ app/scripts/components/custom.js
@@ -2,6 +2,14 @@ import $ from 'jquery'
 
 export default () => {
 
+  if ($(window).width() < 1200) {
+    $('.header__menu-nav-link').on('click', function dropMenuFunc() {
+      $(this).parents('.header__menu-nav-item').toggleClass('active')
+      return false
+    })
+  } 
+
+
   if ($('.article__aside').length) {
     $('.wrapper').addClass('wrapper--ovv')
   }
@@ -16,52 +24,54 @@ export default () => {
     $(this).parents('.faq__item').toggleClass('active')
   })
 
-  const navigationLinks = document.querySelectorAll('.article__nav a');
-  const sidebarMenu = document.querySelector('.article__aside');
+  if ($('.article__aside').length) {
+    const navigationLinks = document.querySelectorAll('.article__nav a');
+    const sidebarMenu = document.querySelector('.article__aside');
+    
+    function isElementVisible(element) {
+      const rect = element.getBoundingClientRect();
+      return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      );
+    }
   
-  function isElementVisible(element) {
-    const rect = element.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-  }
-
-  window.addEventListener('scroll', () => {
-    navigationLinks.forEach(link => {
-      const targetSectionId = link.getAttribute('href').substring(1);
-      const targetSection = document.getElementById(targetSectionId);
-
-      if (isElementVisible(targetSection)) {
-        link.classList.add('active');
+    window.addEventListener('scroll', () => {
+      navigationLinks.forEach(link => {
+        const targetSectionId = link.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetSectionId);
+  
+        if (isElementVisible(targetSection)) {
+          link.classList.add('active');
+        } else {
+          link.classList.remove('active');
+        }
+      });
+      const sidebarMenuRect = sidebarMenu.getBoundingClientRect();
+      if (sidebarMenuRect.top < 0 || sidebarMenuRect.bottom > window.innerHeight) {
+        sidebarMenu.classList.add('hidden');
       } else {
-        link.classList.remove('active');
+        sidebarMenu.classList.remove('hidden');
       }
     });
-    const sidebarMenuRect = sidebarMenu.getBoundingClientRect();
-    if (sidebarMenuRect.top < 0 || sidebarMenuRect.bottom > window.innerHeight) {
-      sidebarMenu.classList.add('hidden');
-    } else {
-      sidebarMenu.classList.remove('hidden');
-    }
-  });
-
-  navigationLinks.forEach(link => {
-    link.addEventListener('click', (event) => {
-      event.preventDefault();
-
-      const targetSectionId = link.getAttribute('href').substring(1);
-      const targetSection = document.getElementById(targetSectionId);
-
-      const targetOffsetTop = targetSection.getBoundingClientRect().top + window.pageYOffset;
-
-      const headerHeight = 110;
-      const targetScrollPosition = targetOffsetTop - headerHeight;
-      window.scrollTo({ top: targetScrollPosition, behavior: 'smooth' });
+  
+    navigationLinks.forEach(link => {
+      link.addEventListener('click', (event) => {
+        event.preventDefault();
+  
+        const targetSectionId = link.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetSectionId);
+  
+        const targetOffsetTop = targetSection.getBoundingClientRect().top + window.pageYOffset;
+  
+        const headerHeight = $(window).width() < 1200 ? 82 : 110;
+        const targetScrollPosition = targetOffsetTop - headerHeight;
+        window.scrollTo({ top: targetScrollPosition, behavior: 'smooth' });
+      });
     });
-  });
+  }
 
   const counters = document.querySelectorAll('.js-count')
 
@@ -89,6 +99,7 @@ export default () => {
   })
   $('.js-close-menu').on('click', () => {
     $('.header').removeClass('open-menu')
+    $('.header__menu-nav-item').removeClass('active')
   })
 
   // $('.header__mobile-menu').on('click', () => {
